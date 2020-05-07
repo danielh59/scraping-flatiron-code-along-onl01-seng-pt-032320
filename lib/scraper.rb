2422b4fcca38eb29f1217af74ffec4200d8b6c87
Daniel Hoyte <daniel.hoyte59@gmail.com>
2020-05-07 22:21:19 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -8,7 +8,6 @@ class Scraper
   def get_page
 html = open("http://learn-co-curriculum.github.io/site-for-scraping/courses")
 NOKOGIRI::HTML(html)
-doc = NOKOGIRI::HTML(html)
   end
 
 def get_courses
